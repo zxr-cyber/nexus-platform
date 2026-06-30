1f611cc2607ccfeda6b37b462182dd2ec5a7024b
zxr-cyber <yawhalfin@gmail.com>
2026-06-30 22:13:49 +0800

    Update vite.config.js

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,3 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
